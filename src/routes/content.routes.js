c8d44ca234143855e0fd95e9b4a35b002237e003
Rolex sir <aghazafzal8294@gmail.com>
2025-12-14 08:55:50 +0530

    Update content.routes.js

--- src/routes/content.routes.js
+++ src/routes/content.routes.js
@@ -4,4 +4,35 @@ const { getUnifiedContent } = require("../controllers/content.controller");
 
 router.get("/content", getUnifiedContent);
 
+// GET movie by slug
+router.get("/movie/:slug", async (req, res) => {
+  try {
+    const movie = await Movie.findOne({ slug: req.params.slug }).lean();
+
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
+    res.json(movie);
+  } catch (e) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// GET tv show by slug
+router.get("/tv/:slug", async (req, res) => {
+  try {
+    const tv = await TvShow.findOne({ slug: req.params.slug }).lean();
+
+    if (!tv) {
+      return res.status(404).json({ message: "TV show not found" });
+    }
+
+    res.json(tv);
+  } catch (e) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
 module.exports = router;
