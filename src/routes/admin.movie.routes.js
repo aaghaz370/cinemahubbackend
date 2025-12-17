000c7afa1a0a39c1ed7d734610f56c9d095c1e16
Rolex sir <aghazafzal8294@gmail.com>
2025-12-17 20:32:33 +0530

    Update admin.movie.routes.js

--- src/routes/admin.movie.routes.js
+++ src/routes/admin.movie.routes.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
-// const auth = require("../middlewares/auth.middleware");
-
 const {
   addMovie,
   updateMovie,
-  deleteMovie
+  deleteMovie,
+
+  addWatchServer,
+  updateWatchServer,
+  deleteWatchServer,
+
+  addDownloadQuality,
+  deleteDownloadQuality,
+  addDownloadServer,
+  updateDownloadServer,
+  deleteDownloadServer
 } = require("../controllers/admin.movie.controller");
 
-// 🔐 ADMIN PROTECTED ROUTES
-// router.post("/admin/movie", auth, addMovie);
-// router.put("/admin/movie/:id", auth, updateMovie);
-// router.delete("/admin/movie/:id", auth, deleteMovie);
-router.post("/admin/movie",  addMovie);
-router.put("/admin/movie/:id",  updateMovie);
-router.delete("/admin/movie/:id",  deleteMovie);
+/* ================= BASIC ================= */
+router.post("/admin/movie", addMovie);
+router.put("/admin/movie/:id", updateMovie);
+router.delete("/admin/movie/:id", deleteMovie);
+
+/* ================= WATCH ================= */
+router.post("/admin/movie/:id/watch", addWatchServer);
+router.put("/admin/movie/:id/watch", updateWatchServer);
+router.delete("/admin/movie/:id/watch", deleteWatchServer);
+
+/* ================= DOWNLOAD ================= */
+router.post("/admin/movie/:id/download/quality", addDownloadQuality);
+router.delete("/admin/movie/:id/download/quality", deleteDownloadQuality);
+
+router.post("/admin/movie/:id/download/server", addDownloadServer);
+router.put("/admin/movie/:id/download/server", updateDownloadServer);
+router.delete("/admin/movie/:id/download/server", deleteDownloadServer);
 
 module.exports = router;
 
